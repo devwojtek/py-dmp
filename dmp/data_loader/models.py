a7d75cb3a12ad484807dea57db0e29499749e9ef
devwojtek <devwojtek99@gmail.com>
2017-05-18 20:53:42 +0300

    upd: yml parser fix

--- dmp/data_loader/models.py
+++ dmp/data_loader/models.py
@@ -50,7 +50,7 @@ class DataSource(models.Model):
         if template:
             with codecs.open(template, 'r', 'utf-8') as fi:
                 template_data = yaml.round_trip_load(fi, preserve_quotes=True)
-            with open(os.path.join(settings.MEDIA_ROOT, self.upload_file.name)) as key_file:
+            with codecs.open(os.path.join(settings.MEDIA_ROOT, self.upload_file.name), 'r', 'utf-8') as key_file:
                 key_data = key_file.read()
             template_data['in']['json_key_content'] = key_data
             template_data['in']['view_id'] = self.account_id
