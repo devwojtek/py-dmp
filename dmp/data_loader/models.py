4d91386aafc9eb0b5c3e445f5b695bbd102a9554
devwojtek <devwojtek99@gmail.com>
2017-05-19 19:02:50 +0300

    upd: yml parser fix

--- dmp/data_loader/models.py
+++ dmp/data_loader/models.py
@@ -69,7 +69,7 @@ class DataSource(models.Model):
             os.makedirs(log_path)
         log_file = open(os.path.join(log_path, 'embulk_stdout.log'), "w+")
         # call("which embulk; embulk".format(embulk_path=settings.EMBULK_USER_PATH), shell=True, stdout=log_file)
-        return call("which embulk; {embulk_path}.embulk/bin/embulk gem list. {embulk_path}.bashrc; {embulk_path}.embulk/bin/embulk run {filename}".format(embulk_path=settings.EMBULK_USER_PATH,
+        return call("which embulk; {embulk_path}.embulk/bin/embulk gem list. {embulk_path}.bashrc; embulk run {filename}".format(embulk_path=settings.EMBULK_USER_PATH,
                                                                             filename=fname), shell=True, stdout=log_file)
 
 
