665f6da25fdc1ca10764e4d55a2416f932ec2c1a
devwojtek <devwojtek99@gmail.com>
2017-08-30 19:17:07 +0300

    Create AdWords script

---
+++ adwords/adwords.sh
@@ -0,0 +1,2 @@
+/var/www/html/.virtualenvs/dmp/bin/python /var/www/html/data-management-platform/adwords/get_data.py
+/home/web-user/.embulk/bin/embulk run /var/www/html/data-management-platform/adwords/adwords.yml
