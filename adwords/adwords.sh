fe71ed9b9133e22ced4f59da95b2d47d98dd9356
devwojtek <devwojtek99@gmail.com>
2017-09-07 10:50:09 +0300

    Improve .gitignore file

--- adwords/adwords.sh
+++ adwords/adwords.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 /var/www/html/.virtualenvs/dmp/bin/python /var/www/html/data-management-platform/adwords/get_data.py
 /home/web-user/.embulk/bin/embulk run /var/www/html/data-management-platform/adwords/adwords.yml
--- fb_ads/embulk.sh
+++ fb_ads/embulk.sh
@@ -1,2 +1,3 @@
-/var/www/html/.virtualenvs/dmp/bin/python /var/www/html/data-management-platform/fb_ads/pulling.py
+#!/bin/bash
+/var/www/html/.virtualenvs/dmp/bin/python /var/www/html/data-management-platform/fb_ads/pulling.py -f /var/www/html/data-management-platform/fb_ads/credentials.json
 /home/web-user/.embulk/bin/embulk run /var/www/html/data-management-platform/fb_ads/embulk_config.yml
